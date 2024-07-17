175a418f7d092b703a9757d408144a6114dbe6df
Amir <salehamir708@gmail.com>
2024-07-18 00:01:29 +0330

    Code : Pos class refers to a handle for position in map

---
+++ Backtracking/Maze/simple_maze_solving_using_backtracking.py
@@ -0,0 +1,7 @@
+class Pos:
+    def __init__(self, row, col):
+        self.row = row
+        self.col = col
+
+    def __eq__(self, other):
+        return self.row == other.row and self.col == other.col
