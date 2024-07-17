4c0434a893750f0f259f9eb006d6dc146baec709
Amir <salehamir708@gmail.com>
2024-07-18 00:09:26 +0330

    Class attribute : Pos class constructor type-specifier is modified to avoid some warnnings

--- Backtracking/Maze/simple_maze_solving_using_backtracking.py
+++ Backtracking/Maze/simple_maze_solving_using_backtracking.py
@@ -1,7 +1,9 @@
 class Pos:
-    def __init__(self, row, col):
+    def __init__(self, row: int, col: int):
         self.row = row
         self.col = col
 
     def __eq__(self, other):
         return self.row == other.row and self.col == other.col
+
+
