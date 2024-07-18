ee6856e1728fda5c2b03972bebcbb35afb71b8a9
Amir <salehamir708@gmail.com>
2024-07-18 16:05:44 +0330

    this way is not good solving maze, I will try to use vector

--- Backtracking/Maze/simple_maze_solving_using_backtracking.py
+++ Backtracking/Maze/simple_maze_solving_using_backtracking.py
@@ -1,3 +1,4 @@
+import time
 class Pos:
     def __init__(self, row: int, col: int):
         self.row = row
@@ -28,6 +29,12 @@ class Maze:
     def getChar(self, position: Pos):
         return self.map[position.row][position.col]
 
+    def printMaze(self):
+        for row in self.map:
+            for item in row:
+                print(item, end= ' ')
+            print()
+
 
 class Seeker:
     def __init__(self, initPos: Pos, maze: Maze):
@@ -42,22 +49,22 @@ class Seeker:
     def checkRightMove(self):
         width = self.map.getWidth()
         nextPos = Pos(self.current.row, self.current.col+1)
-        return nextPos.col != (width - 1) and (self.map.getChar(nextPos) == '_' or self.map.getChar(nextPos) == 'D')
+        return self.current.col != (width - 1) and (self.map.getChar(nextPos) == '_' or self.map.getChar(nextPos) == 'D')
 
     def checkLeftMove(self):
         width = self.map.getWidth()
         nextPos = Pos(self.current.row, self.current.col - 1)
-        return nextPos.col != 0 and (self.map.getChar(nextPos) == '_' or self.map.getChar(nextPos) == 'D')
+        return self.current.col != 0 and (self.map.getChar(nextPos) == '_' or self.map.getChar(nextPos) == 'D')
 
     def checkDownMove(self):
         height = self.map.getHeight()
         nextPos = Pos(self.current.row + 1, self.current.col)
-        return nextPos.row != (height - 1) and (self.map.getChar(nextPos) == '_' or self.map.getChar(nextPos) == 'D')
+        return self.current.row != (height - 1) and (self.map.getChar(nextPos) == '_' or self.map.getChar(nextPos) == 'D')
 
     def checkUpMove(self):
         height = self.map.getHeight()
         nextPos = Pos(self.current.row - 1, self.current.col)
-        return nextPos.row != 0 and (self.map.getChar(nextPos) == '_' or self.map.getChar(nextPos) == 'D')
+        return self.current.row != 0 and (self.map.getChar(nextPos) == '_' or self.map.getChar(nextPos) == 'D')
 
     def getActionList(self):
         return self.actionList
@@ -72,5 +79,109 @@ class Seeker:
         return False
 
     def addPosToThread(self, newPos: Pos):
-        if self.searchInThread(newPos):
             self.thread.append(newPos)
+
+
+
+def solve(seeker:Seeker, currentPos: Pos, maze: Maze):
+
+    thread = seeker.thread
+
+    if maze.getChar(seeker.current) == 'D':
+        return True
+
+    if seeker.current == currentPos and seeker.actionList != []:
+        return False
+
+    else:
+
+        if seeker.checkRightMove():
+            nextPos = Pos(seeker.current.row, seeker.current.col + 1)
+            if seeker.searchInThread(nextPos):
+                maze.setChar(nextPos, '◉')
+                maze.setChar(seeker.current, '#')
+                seeker.current = nextPos
+            else:
+                seeker.addPosToThread(nextPos)
+                maze.setChar(seeker.current, '_')
+                seeker.current = nextPos
+                maze.setChar(nextPos, '◉')
+            maze.printMaze()
+            print("----------------------------------------")
+            time.sleep(0.5)
+            return solve(seeker, seeker.current, maze)
+
+
+        elif seeker.checkDownMove():
+            nextPos = Pos(seeker.current.row + 1, seeker.current.col)
+            if seeker.searchInThread(nextPos):
+                maze.setChar(nextPos, '◉')
+                maze.setChar(seeker.current, '#')
+                seeker.current = nextPos
+            else:
+                seeker.addPosToThread(nextPos)
+                maze.setChar(seeker.current, '_')
+                seeker.current = nextPos
+                maze.setChar(nextPos, '◉')
+
+            maze.printMaze()
+            print("----------------------------------------")
+
+            time.sleep(0.5)
+            return solve(seeker, seeker.current, maze)
+
+
+        elif seeker.checkLeftMove():
+            nextPos = Pos(seeker.current.row, seeker.current.col - 1)
+            if seeker.searchInThread(nextPos):
+                maze.setChar(nextPos, '◉')
+                maze.setChar(seeker.current, '#')
+                seeker.current = nextPos
+            else:
+                seeker.addPosToThread(nextPos)
+                maze.setChar(seeker.current, '_')
+                seeker.current = nextPos
+                maze.setChar(nextPos, '◉')
+
+
+            maze.printMaze()
+            print("----------------------------------------")
+            time.sleep(0.5)
+            return solve(seeker, seeker.current, maze)
+
+
+        elif seeker.checkUpMove():
+            nextPos = Pos(seeker.current.row - 1, seeker.current.col)
+            if seeker.searchInThread(nextPos):
+                maze.setChar(nextPos, '◉')
+                maze.setChar(seeker.current, '#')
+                seeker.current = nextPos
+            else:
+                seeker.addPosToThread(nextPos)
+                maze.setChar(seeker.current, '_')
+                seeker.current = nextPos
+                maze.setChar(nextPos, '◉')
+
+
+            maze.printMaze()
+            print("----------------------------------------")
+            time.sleep(0.5)
+            return solve(seeker, seeker.current, maze)
+
+
+        else:
+            return False
+
+
+map = [
+    ['_', '_', '_', '_', '_'],
+    ['#', '#', '_', '_', '#'],
+    ['#', '_', '_', '#', '_'],
+    ['_', '_', '#', '#', '#'],
+    ['_', '_', '_', '_', 'D']
+]
+
+maze = Maze(map, 5, 5)
+maze.printMaze()
+seeker = Seeker(Pos(0, 0), maze)
+solve( seeker ,Pos(0, 0), maze)
