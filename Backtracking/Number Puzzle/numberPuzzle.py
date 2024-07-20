1630d478b7060b432fa11e5e4030cb4fea91ed4b
Amir <salehamir708@gmail.com>
2024-07-20 13:38:15 +0330

    Class Board : is updated using new mehtods for moving board pieces and is_sorted which is used for end of looking for solution

--- Backtracking/Number Puzzle/numberPuzzle.py
+++ Backtracking/Number Puzzle/numberPuzzle.py
@@ -12,3 +12,33 @@ class Board:
                 print(number, end=' ')
             print()
         sleep(2.3)
+
+    def moveRight(self, row, col): # row and col of empty cell of map
+        temp = self.map[row][col]
+        self.map[row][col] = self.map[row][col + 1]
+        self.map[row][col + 1] = temp
+
+    def moveLeft(self, row, col): # row and col of empty cell of map
+        temp = self.map[row][col]
+        self.map[row][col] = self.map[row][col - 1]
+        self.map[row][col - 1] = temp
+
+    def moveDown(self, row, col): # row and col of empty cell of map
+        temp = self.map[row][col]
+        self.map[row][col] = self.map[row + 1][col]
+        self.map[row + 1][col] = temp
+
+    def moveUp(self, row, col): # row and col of empty cell of map
+        temp = self.map[row][col]
+        self.map[row][col] = self.map[row - 1][col]
+        self.map[row - 1][col] = temp
+
+    def is_sorted(self):
+        line = []
+        for row in self.map:
+            for number in row:
+                if number != ' ':
+                    line.append(number)
+        base = line[:]
+        base.sort()
+        return base == line
