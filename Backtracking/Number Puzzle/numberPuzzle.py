bd13be45091bec8d29b913f0f0ebf6d045936fe6
Amir <salehamir708@gmail.com>
2024-07-20 13:24:39 +0330

    Class Board : is added to project for game Board

--- Backtracking/Number Puzzle/numberPuzzle.py
+++ Backtracking/Number Puzzle/numberPuzzle.py
@@ -1,3 +1,14 @@
 from time import sleep
 
 
+class Board:
+    def __init__(self, N: int, map: list[list]): # Constraints : Board has square shape
+        self.boardSize = N
+        self.map = map
+
+    def printMap(self):
+        for row in self.map:
+            for number in row:
+                print(number, end=' ')
+            print()
+        sleep(2.3)
